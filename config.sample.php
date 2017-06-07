7b8ce251b88126ca9fe43b71f5c2de77c1b15f7c
Jarred Allen <JarredAllen@users.noreply.github.com>
2017-06-07 13:34:51 -0700

    I forgot to include the new sample config file

---
+++ config.sample.php
@@ -0,0 +1,9 @@
+<?php
+	//sample file
+	//not actual login credentials
+	define('view_credentials', array('view', "password"));
+	
+	define('insert_credentials', array('insert', "password"));
+	
+	define('modify_credentials', array('modify', "password"));
+?>
