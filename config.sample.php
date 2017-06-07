af090d81fdcc6c2cd6473afc9447760c25e1ce4d
Jarred Allen <JarredAllen@users.noreply.github.com>
2017-06-07 14:42:37 -0700

    Delete config.sample.php

--- config.sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//sample file
-	//not actual login credentials
-	define('view_credentials', array('view', "password"));
-	
-	define('insert_credentials', array('insert', "password"));
-	
-	define('modify_credentials', array('modify', "password"));
-?>
